28faa4f8d382c33149f981e6a7165a0b6cd4c333
ashay0072001 <ashaykumar2001@gmail.com>
2022-10-21 12:13:14 +0530

    fix

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function multiOfInitialAndQuantity(initial,quantity,current){
      if(initial > current){
         var loss = (initial-current) * quantity;
         var lossPercentage = (loss /initial) * 100; 
-        showOutput(`sad to know that your loss is ${loss} and percentage is ${lossPercentage} %` );
+        showOutput(`sad to know that your loss is ${loss} and percentage is ${Math.abs(lossPercentage)} %` );
      }
      else if(current > initial){
          var profit = (current - initial) * quantity;
          var profitPercentage = (profit / initial) * 100;
-         showOutput(`Good to know that your profit is ${profit} and percentage is ${profitPercentage} %`)
+         showOutput(`Good to know that your profit is ${profit} and percentage is ${Math.abs(profitPercentage)} %`)
          
      }else{
          showOutput(`😑 😑 😑 Enter the value in boxes or your grandma will serve all your cookies 😑 😑 😑`);
